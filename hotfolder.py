6a2d0c2d8d2eaecc3358c2496e49d95d6f387abf
Carlos Amedee <camedee@gmail.com>
2010-11-17 09:28:24 -0500

    Added a configuration validator. Started to add a cli interface.

--- hotfolder.py
+++ hotfolder.py
@@ -3,9 +3,13 @@ import sys
 import shutil
 import time
 
+# NOTES
+# recursive dir vs static folder.
+# check for a specific version of python
+
 class Hotfolder(object):
     
-  def __init__(self, source_path, dest_path, cycles, wait, os, label):
+  def __init__(self, source_path, dest_path, cycles, wait, label):
     print 20*"*"
     print "Starting hotfolder - " + label + "...."
     self._source_path = source_path
@@ -16,13 +20,11 @@ class Hotfolder(object):
     self._file_hash = {}
     self._delete_source = False
     self._name = label
-    # NOTES
-    # Check all conditions on startup.
-    # recursive dir vs static folder.
-    # make it run via cli
-    # check that source and dest are diffrent
-    # Print initial configuration
-    # check for a specific version of python
+    self.print_configuration()
+    self.validate_configuration()
+    print "Initialization complete."
+
+  def print_configuration(self):
     print "Hotfolder configuration:"
     print "Source: " + str(source_path)
     print "Destination: " + str(dest_path)
@@ -31,8 +33,24 @@ class Hotfolder(object):
     print "System Platform: " + sys.platform + " - " + sys.version
     print "Hotfolder name: " + str(label)
     print "Delete source: " + str(self._delete_source)
-    print 20*"*"
-    print "Initialization complete."
+    print 20*"*" 
+
+  def validate_configuration(self):
+    # Check file paths
+    if self._source_path == self._dest_path:
+      print "Configuration error: The source and destination can't be the same."
+      exit(0)
+    if (not os.path.exists(self._source_path) or not os.path.isdir(self._source_path)):
+      print not os.path.exists(self._source_path)
+      print "Configuration error: The source directory is not valid."
+      exit(0)
+    if (not os.path.exists(self._dest_path) or not os.path.isdir(self._dest_path)):
+      print "Configuration error: The destination directory is not valid."
+      exit(1)
+    print "\"%s\"" % (self._source_path)
+    if (self._source_path == "" or self._dest_path == ""):
+      print "Configuration error: The paths are invalid"
+      exit(0)
 
   def set_delete_source(self, val):
     self._delete_source = val
@@ -85,17 +103,16 @@ class Hotfolder(object):
         self._file_hash[file] = [size]
         return False
 
-
-
 src = ""
 dst = ""
 
-try:
-  test = Hotfolder(src, dst, 3, 5, "linux", "Test Hotfolder")
-  test.set_delete_source(True)
-  test.process_continuous()
-except (KeyboardInterrupt, SystemExit):
-  print "\nApplication terminated via keyboard"
+if __name__ == '__main__':
+  try:
+    test = Hotfolder(src, dst, 3, 5, "Test Hotfolder")
+    test.set_delete_source(True)
+    test.process_continuous()
+  except (KeyboardInterrupt, SystemExit):
+    print "\nApplication terminated"
 
 
 
